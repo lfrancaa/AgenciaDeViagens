1d009ce019d88cb5cfdbda4643965ca06d4fdb23
Luciano França <dv.lfrancaa@outlook.com>
2022-11-06 16:48:17 -0300

    Retirada de Arquivos Desnecessários

--- AgenciaDeViagens_MVC_Dot_Net/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AgenciaDeViagens_MVC_Dot_Net.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
